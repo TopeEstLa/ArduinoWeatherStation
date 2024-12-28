2b475e9dd6b7dcaab77484d6d1a834dd61bbfb6f
TopeEstLa <contact.tope95@gmail.com>
2024-12-28 17:56:41 +0100

    add temp average between dht11 & dht22

--- weatherstation.ino
+++ weatherstation.ino
@@ -3,7 +3,9 @@
 #include <DHT.h>
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
-DHT dht(4, DHT22);
+
+DHT dht_11(2, DHT11);
+DHT dht_22(4, DHT22);
 
 float temp;
 float hum;
@@ -12,20 +14,28 @@ void setup() {
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  dht.begin();
+  dht_11.begin();
+  dht_22.begin();
 }
 
 void loop() {
-  hum = dht.readHumidity();
-  temp = dht.readTemperature();
+  dht_22 = dht_22.readHumidity(); //Only get humidity from the DHT 22
+
+
+  float temp_11 = dht_11.readTemperature();
+  float temp_22 = dht_22.readTemperature();
+
+  temp = temp_11 + temp_22 / 2 //take the average between DHT11 & DHT22 
 
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
+  lcd.print("Temperature: ");
   lcd.print(temp);
+  lcd.print(" c");
   lcd.setCursor(0, 1);
-  lcd.print("Hum: ");
+  lcd.print("Humidity: ");
   lcd.print(temp);
+  lcd.print(" %");
 
   delay(2000);
 }
